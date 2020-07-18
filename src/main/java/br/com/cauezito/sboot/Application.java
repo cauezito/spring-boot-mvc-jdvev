ba58be001b16b929d23a2199a5fbfde9831b3aa7
Cauê Santos <caah.sfc@live.com>
2020-07-18 14:15:46 -0300

    Configurando JPA e banco de dados

--- src/main/java/br/com/cauezito/sboot/Application.java
+++ src/main/java/br/com/cauezito/sboot/Application.java
@@ -2,8 +2,10 @@ package br.com.cauezito.sboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "br.com.cauezito.model")
 public class Application {
 
 	public static void main(String[] args) {
